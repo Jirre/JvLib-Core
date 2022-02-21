a1a1e3189ecdc182fe20a60fa9fcc241d9dd65b2
Jirrev <jirreamon@gmail.com>
2022-02-22 00:30:26 +0100

    [/] Fixed event namespace

--- Scripts/Runtime/Events/SafeEvent.cs
+++ Scripts/Runtime/Events/SafeEvent.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using Object = UnityEngine.Object;
 
-namespace JvLib.Collections
+namespace JvLib.Events
 {
     public abstract class SafeEventBase
     {
