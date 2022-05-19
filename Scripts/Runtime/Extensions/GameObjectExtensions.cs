24b120afff7b1cc8926399dee3a0565098797da6
Jirre Verkerk <jirreamon@gmail.com>
2022-05-19 15:41:04 +0200

    [/] LayerMaskExtension spread over correct extension files

--- Scripts/Runtime/Extensions/GameObjectExtensions.cs
+++ Scripts/Runtime/Extensions/GameObjectExtensions.cs
@@ -18,5 +18,14 @@ namespace UnityEngine
                 c = gameObject.AddComponent<T>();
             return c;
         }
+        
+        /// <summary>
+        ///   <para>Is this game object tagged with layer mask ?</para>
+        /// </summary>
+        /// <param name="layerMask">The layer mask to compare.</param>
+        public static bool CompareLayer(this GameObject gameObject, LayerMask layerMask)
+        {
+            return (layerMask.value & (1 << gameObject.layer)) > 0;
+        }
     }
 }
